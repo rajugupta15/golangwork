a67f2e292daa35d25d8acb8ee2ce69a83808ce81
Raju <rajugupta15@gmail.com>
2017-12-13 14:25:20 +0530

    updated variable in reverse order number example

--- src/reverseOrder/reverseorder.go
+++ src/reverseOrder/reverseorder.go
@@ -2,15 +2,15 @@ package main
 
 import "fmt"
 
-var number int = 123
+var number int = 123456789
 var reverse int = 0
 var sd int = 0
 
 func main() {
-	for ;number > 0; {
+	for number > 0 {
 		sd = number % 10
-		reverse = reverse * 10 + sd
-		number = number/10
+		reverse = reverse*10 + sd
+		number = number / 10
 	}
 	fmt.Println(reverse)
 }
