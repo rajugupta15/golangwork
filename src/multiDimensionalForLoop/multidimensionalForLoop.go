a6acc171bbd7774e2105215182147f9375c9a503
Raju <rajugupta15@gmail.com>
2017-12-13 14:23:40 +0530

    add multi dimensional for loop example

---
+++ src/multiDimensionalForLoop/multidimensionalForLoop.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+var (
+	domain = []string{"plivo", "plivops"}
+)
+
+func main() {
+	for i := 0; i < 12; i++ {
+		for j := 0; j < 2; j++ {
+			fmt.Println(domain[j] + strconv.Itoa(i+1))
+		}
+	}
+}
