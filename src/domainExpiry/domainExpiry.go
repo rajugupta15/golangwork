06d51cfa0c41f9e9b2e16f4572028a3bba42bdfb
Raju Gupta <rajugupta15@gmail.com>
2017-12-15 22:35:57 +0530

    Update domainExpiry.go

--- src/domainExpiry/domainExpiry.go
+++ src/domainExpiry/domainExpiry.go
@@ -1,10 +1,33 @@
 package main
 
 import (
+	// "bytes"
+
 	"fmt"
-	"time"
+	"os/exec"
+	"strings"
 )
 
 func main() {
-	fmt.Println(time.Now())
+	// path, err := exec.LookPath("whois")
+	cmd, err := exec.Command("whois", "plivo.com").Output()
+	// fmt.Println(cmd)
+	if err != nil {
+		fmt.Println(err)
+	}
+	str := fmt.Sprintf("%s", cmd)
+	strArray := strings.Split(str, "\n")
+	// fmt.Println(strArray[6])
+	// fmt.Printf("%q\n", strings.SplitN(str, "Registry Expiry Date:", 2))
+	b := contains(strArray, "Registry")
+	fmt.Println(b)
+}
+
+func contains(arr []string, str string) bool {
+	for _, a := range arr {
+		if a == str {
+			return true
+		}
+	}
+	return false
 }
