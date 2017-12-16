db91f4d2e55500782e4324e0d70dc8125e1d5feb
Raju Gupta <rajugupta15@gmail.com>
2017-12-16 23:05:50 +0530

    Update domainExpiry.go

--- src/domainExpiry/domainExpiry.go
+++ src/domainExpiry/domainExpiry.go
@@ -1,33 +1,40 @@
 package main
 
 import (
-	// "bytes"
-
 	"fmt"
 	"os/exec"
 	"strings"
+	"time"
 )
 
-func main() {
-	// path, err := exec.LookPath("whois")
-	cmd, err := exec.Command("whois", "plivo.com").Output()
-	// fmt.Println(cmd)
-	if err != nil {
-		fmt.Println(err)
+func handler(b error) error {
+	if b != nil {
+		fmt.Println(b)
 	}
+	return b
+}
+func domainExpiry(domain string) string {
+	cmd, err := exec.Command("whois", domain).Output()
+	handler(err)
 	str := fmt.Sprintf("%s", cmd)
 	strArray := strings.Split(str, "\n")
-	// fmt.Println(strArray[6])
-	// fmt.Printf("%q\n", strings.SplitN(str, "Registry Expiry Date:", 2))
-	b := contains(strArray, "Registry")
-	fmt.Println(b)
+	s := strings.Split(strArray[6], ":")
+	expiry := s[1:]
+	format := strings.Join(expiry, ":")
+	trim := strings.TrimSpace(format)
+	return (trim)
 }
-
-func contains(arr []string, str string) bool {
-	for _, a := range arr {
-		if a == str {
-			return true
-		}
-	}
-	return false
+func timeFormat(t string) time.Time {
+	t1, e := time.Parse(time.RFC3339, t)
+	handler(e)
+	s := time.Date(t1.Year(), t1.Month(), t1.Day(), 0, 0, 0, 0, time.UTC)
+	return s
+}
+func main() {
+	domain := "thegeeklinux.com"
+	a := domainExpiry(domain)
+	b := timeFormat(a)
+	c := time.Now()
+	duration := b.Sub(c)
+	fmt.Println(domain, "will expire in", int(duration.Hours()/24), "days")
 }
